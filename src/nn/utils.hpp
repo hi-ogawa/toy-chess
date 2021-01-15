39afad56216a89d85229ef5fb5bf92da2b21cc75
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2021-01-15 15:31:18 +0900

    Implement USE_SIMD option

--- src/nn/utils.hpp
+++ src/nn/utils.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
 #include "../misc.hpp"
-#include <immintrin.h> // AVX, FMA
+#include <config.hpp>
+
+#ifdef USE_SIMD
+  #include <immintrin.h> // AVX, FMA
+#endif
 
 namespace nn {
 
@@ -14,93 +18,93 @@ template<int N>
 void relu(Float x[N], Float y[N]) {
   static_assert(N % 8 == 0);
 
-  // [ Normal ]
-  // for (int i = 0; i < N; i++) {
-  //   y[i] = std::max(x[i], 0.0f);
-  // }
-
-  // [ AVX ]
-  constexpr __m256 kZero = {0, 0, 0, 0, 0, 0, 0, 0};
-  for (int i = 0; i < N; i += 8) {
-    auto v = _mm256_load_ps(&x[i]);
-    v = _mm256_max_ps(v, kZero);
-    _mm256_store_ps(&y[i], v);
-  }
+  #ifdef USE_SIMD
+    constexpr __m256 kZero = {0, 0, 0, 0, 0, 0, 0, 0};
+    for (int i = 0; i < N; i += 8) {
+      auto v = _mm256_load_ps(&x[i]);
+      v = _mm256_max_ps(v, kZero);
+      _mm256_store_ps(&y[i], v);
+    }
+  #else
+    for (int i = 0; i < N; i++) {
+      y[i] = std::max(x[i], 0.0f);
+    }
+  #endif
 }
 
 template<int N>
 inline Float dot(Float x[N], Float y[N]) {
   static_assert(N % kSimdWidth == 0);
 
-  // [ Normal ]
-  // Float res = 0;
-  // for (int i = 0; i < N; i++) {
-  //   res += x[i] * y[i];
-  // }
-  // return res;
-
-  // [ AVX, FMA ]
-  __m256 res = {0, 0, 0, 0, 0, 0, 0, 0};
-  for (int i = 0; i < N; i += 8) {
-    auto vx = _mm256_load_ps(&x[i]);
-    auto vy = _mm256_load_ps(&y[i]);
-    res = _mm256_fmadd_ps(vx, vy, res);
-  }
-  res = _mm256_hadd_ps(res, res);
-  res = _mm256_hadd_ps(res, res);
-  return res[0] + res[5];
+  #ifdef USE_SIMD
+    __m256 res = {0, 0, 0, 0, 0, 0, 0, 0};
+    for (int i = 0; i < N; i += 8) {
+      auto vx = _mm256_load_ps(&x[i]);
+      auto vy = _mm256_load_ps(&y[i]);
+      res = _mm256_fmadd_ps(vx, vy, res);
+    }
+    res = _mm256_hadd_ps(res, res);
+    res = _mm256_hadd_ps(res, res);
+    return res[0] + res[5];
+  #else
+    Float res = 0;
+    for (int i = 0; i < N; i++) {
+      res += x[i] * y[i];
+    }
+    return res;
+  #endif
 }
 
 template<int N>
 inline void copy(Float x[N], Float y[N]) {
   static_assert(N % kSimdWidth == 0);
 
-  // [ Normal ]
-  // for (int i = 0; i < N; i++) {
-  //   y[i] = x[i];
-  // }
-
-  // [ AVX ]
-  for (int i = 0; i < N; i += 8) {
-    auto v = _mm256_load_ps(&x[i]);
-    _mm256_store_ps(&y[i], v);
-  }
+  #ifdef USE_SIMD
+    for (int i = 0; i < N; i += 8) {
+      auto v = _mm256_load_ps(&x[i]);
+      _mm256_store_ps(&y[i], v);
+    }
+  #else
+    for (int i = 0; i < N; i++) {
+      y[i] = x[i];
+    }
+  #endif
 }
 
 template<int N>
 inline void add(Float x[N], Float y[N], Float z[N]) {
   static_assert(N % kSimdWidth == 0);
 
-  // [ Normal ]
-  // for (int i = 0; i < N; i++) {
-  //   z[i] = x[i] + y[i];
-  // }
-
-  // [ AVX ]
-  for (int i = 0; i < N; i += 8) {
-    auto vx = _mm256_load_ps(&x[i]);
-    auto vy = _mm256_load_ps(&y[i]);
-    auto vz = _mm256_add_ps(vx, vy);
-    _mm256_store_ps(&z[i], vz);
-  }
+  #ifdef USE_SIMD
+    for (int i = 0; i < N; i += 8) {
+      auto vx = _mm256_load_ps(&x[i]);
+      auto vy = _mm256_load_ps(&y[i]);
+      auto vz = _mm256_add_ps(vx, vy);
+      _mm256_store_ps(&z[i], vz);
+    }
+  #else
+    for (int i = 0; i < N; i++) {
+      z[i] = x[i] + y[i];
+    }
+  #endif
 }
 
 template<int N>
 inline void sub(Float x[N], Float y[N], Float z[N]) {
   static_assert(N % kSimdWidth == 0);
 
-  // [ Normal ]
-  // for (int i = 0; i < N; i++) {
-  //   z[i] = x[i] - y[i];
-  // }
-
-  // [ AVX ]
-  for (int i = 0; i < N; i += 8) {
-    auto vx = _mm256_load_ps(&x[i]);
-    auto vy = _mm256_load_ps(&y[i]);
-    auto vz = _mm256_sub_ps(vx, vy);
-    _mm256_store_ps(&z[i], vz);
-  }
+  #ifdef USE_SIMD
+    for (int i = 0; i < N; i += 8) {
+      auto vx = _mm256_load_ps(&x[i]);
+      auto vy = _mm256_load_ps(&y[i]);
+      auto vz = _mm256_sub_ps(vx, vy);
+      _mm256_store_ps(&z[i], vz);
+    }
+  #else
+    for (int i = 0; i < N; i++) {
+      z[i] = x[i] - y[i];
+    }
+  #endif
 }
 
 template<int N1, int N2>
