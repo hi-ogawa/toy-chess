3598a0270c6fc541b406e0e79f1570b83bd31ca7
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2021-01-18 01:03:11 +0900

    Update misc

--- misc/match/run.sh
+++ misc/match/run.sh
@@ -5,15 +5,16 @@ TIMESTAMP=$(date '+%F-%H-%M-%S')
 CLI=${CLI:-"cutechess-cli"}
 
 CONCURRENCY=${CONCURRENCY:-2}
-ROUNDS=${ROUNDS:-5}
+ROUNDS=${ROUNDS:-20}
 TC=${TC:-"10+0.1"}
 DRAW=${DRAW:-"-draw movenumber=40 movecount=4 score=10"}
 RESIGN=${RESIGN:-"-resign movecount=4 score=1000"}
-RESULT=${RESULT:-"-pgnout misc/match/data/result-$TIMESTAMP.pgn"}
 OPENINGS=${OPENINGS:-"-openings file=misc/match/data/8moves_v3.pgn policy=round -repeat"}
-MORE_OPTIONS=${MORE_OPTIONS} # For "-debug" etc...
 
-SF_ELO=${SF_ELO:-1500}
+RESULT=${RESULT:-"misc/match/data/result-$TIMESTAMP.pgn"}
+LOG=${LOG:-"misc/match/data/log-$TIMESTAMP.txt"}
+
+SF_ELO=${SF_ELO:-1700}
 
 ENGINE1=${ENGINE1:-"name=toy-chess cmd=build/Release/main"}
 ENGINE2=${ENGINE2:-"name=stockfish cmd=stockfish dir=thirdparty/Stockfish/src option.UCI_LimitStrength=true option.UCI_Elo=$SF_ELO"}
@@ -24,4 +25,6 @@ $CLI \
   -concurrency $CONCURRENCY \
   -engine tc=$TC $ENGINE1 -engine tc=$TC $ENGINE2 -each proto=uci \
   -rounds $ROUNDS -games 2 \
-  $OPENINGS $DRAW $RESIGN $RESULT $MORE_OPTIONS
+  -pgnout $RESULT \
+  -debug \
+  $OPENINGS $DRAW $RESIGN |& tee $LOG | grep -v -P '^\d'
