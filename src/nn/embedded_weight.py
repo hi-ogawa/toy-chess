d7d1d33dcb7f9740be50136ed4729245e26f26d3
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2021-01-23 20:24:28 +0900

    Update embedded_weight.py (Support python version < 3.8)

--- src/nn/embedded_weight.py
+++ src/nn/embedded_weight.py
@@ -1,10 +1,19 @@
+#
+# Generate embedded_weight.cpp (see CMakeFile.txt for how this script is used)
+#
 
+import sys
 
 def main(infile):
   with open(infile, 'rb') as f:
     data = f.read()
   size = len(data)
-  data_literal = ('+' + data.hex('+')).replace('+', '\\x')
+  # NOTE: bytes.hex(<seperator>) is from python 3.8
+  if sys.version_info >= (3, 8):
+    data_literal = ('+' + data.hex('+')).replace('+', '\\x')
+  else:
+    h = data.hex()
+    data_literal = ''.join('\\x' + x + y for x, y in zip(h[0::2], h[1::2]))
   print("namespace nn {")
   print("  extern const char* kEmbeddedWeight;")
   print("  extern const int kEmbeddedWeightSize;")
